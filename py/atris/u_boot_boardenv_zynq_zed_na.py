4d0c42bd9efd2f45e90adfe15b0dcfe51646e533
Liam Beguin <liambeguin@gmail.com>
2018-02-26 21:55:56 -0500

    py: atris: add config for sf testing
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- py/atris/u_boot_boardenv_zynq_zed_na.py
+++ py/atris/u_boot_boardenv_zynq_zed_na.py
@@ -4,3 +4,33 @@
 
 
 env__spl_skipped = True
+
+env__sf_configs = (
+    {
+        'offset':  0x00000000,
+        'speed': 10000000,
+        'writeable': True,
+    },
+    {
+        'offset':  0x00000000,
+        'len':  0x00040000,
+        'speed': [10000000, 100000000],
+        'writeable': True,
+    },
+    {
+        'offset':  0x00010000,
+        'writeable': False,
+    },
+    {
+        'offset':  0x00020000,
+        'writeable': True,
+    },
+    {
+        'offset':  0x00030000,
+        'writeable': True,
+    },
+    {
+        'offset':  0x00040000,
+        'writeable': True,
+    },
+)
